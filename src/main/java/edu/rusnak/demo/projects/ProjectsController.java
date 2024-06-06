9767d51066ee4a3cfc6ce88a9ed0432b81e7c0c4
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-06-06 20:13:57 +0300

    [5] added separate controllers for a few sidebar menu items

---
+++ src/main/java/edu/rusnak/demo/projects/ProjectsController.java
@@ -0,0 +1,16 @@
+package edu.rusnak.demo.projects;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectsController {
+
+    @GetMapping
+    public String projects(Model model) {
+        return "projects/list";
+    }
+}
